8512631b4c79c703509d604cf5513aa339005eca
mutilaila <mutiaralaila161-ctrl>
2026-04-26 19:37:33 +0700

    up

--- app/Views/layouts/main.php
+++ app/Views/layouts/main.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PERPUSApp</title>
+    <title>blibiotrack</title>
 
     <!-- Bootstrap -->
     <link href="<?= base_url('assets/css/bootstrap.min.css') ?>" rel="stylesheet">
